433879d46a677f876047c6acadfd389bcb0d5f11
Gizeta <0w0@gizeta.tk>
2016-01-21 00:38:05 +0900

    remove babel polyfill

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import server from './server';
 import config from './config';
 import logger from './logger';
-import 'babel-polyfill';
 
 server.listen(config.port);
 logger.info('kcsp server started.');
